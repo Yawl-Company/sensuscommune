08884e745a916f8b9176a23ce9656e260c6df1f8
Bob Nicholson <bobniklv@gmail.com>
2024-08-11 22:55:44 +0300

    chore: use apply false in root project build.gradle.kts plugins enumeration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.room)
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.room) apply false
 }
