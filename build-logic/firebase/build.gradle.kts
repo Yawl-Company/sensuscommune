576ad6985f837157d20e1fb338476ea5d819ba58
Bob Nicholson <bobniklv@gmail.com>
2024-08-17 00:23:09 +0300

    feat: add firebase convention plugin

---
+++ build-logic/firebase/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm")
+    `java-gradle-plugin`
+}
+
+dependencies {
+    implementation(project(":gradle-extension"))
+    // workaround for https://github.com/gradle/gradle/issues/15383
+    implementation(files((libs as Any).javaClass.superclass.protectionDomain.codeSource.location))
+}
+
+gradlePlugin {
+    plugins {
+        create("firebase") {
+            id = "convention.firebase"
+            implementationClass = "com.yawl.FirebaseConventionPlugin"
+        }
+    }
+}
---
+++ build-logic/firebase/src/main/kotlin/com/yawl/FirebaseConventionPlugin.kt
@@ -0,0 +1,18 @@
+package com.yawl
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FirebaseConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            plugins.apply("com.google.gms.google-services")
+
+            val bom = libs.firebase.bom.get()
+            dependencies.apply {
+                add("implementation", platform(bom))
+                add("implementation", libs.firebase.analytics.get())
+            }
+        }
+    }
+}
