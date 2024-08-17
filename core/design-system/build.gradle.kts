3c556dd8c2c243f6cdbb9b2ed2314ceeb7ce27b6
Bob Nicholson <bobniklv@gmail.com>
2024-08-17 19:29:39 +0300

    feat: add google font provider, update theme

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -8,9 +8,12 @@ android {
 }
 
 dependencies {
+    api(libs.androidx.core.ktx)
     api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.material.iconsExtended)
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.runtime)
     api(libs.androidx.compose.ui.util)
+
+    implementation(libs.androidx.ui.text.google.fonts)
 }
