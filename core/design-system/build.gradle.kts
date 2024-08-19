ce4b3bb555c5c29e6931a197a6971c236cd2dca9
Bob Nicholson <bobniklv@gmail.com>
2024-08-19 19:46:56 +0300

    chore: add androidx material3 adaptive libraries to libs catalog

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -9,11 +9,9 @@ android {
 
 dependencies {
     api(libs.androidx.core.ktx)
-    api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.material.iconsExtended)
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.runtime)
-    api(libs.androidx.compose.ui.util)
 
     implementation(libs.androidx.ui.text.google.fonts)
 }
