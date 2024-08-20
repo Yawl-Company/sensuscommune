57e32fac4f0881c37e0084f18a99625f5a99032d
Bob Nicholson <bobniklv@gmail.com>
2024-08-20 19:37:05 +0300

    refactor: remove kotlin-dsl from signing convention plugins project

--- build-logic/signing/src/main/kotlin/com/yawl/AndroidApplicationSignConventionPlugin.kt
+++ build-logic/signing/src/main/kotlin/com/yawl/AndroidApplicationSignConventionPlugin.kt
@@ -3,12 +3,10 @@ package com.yawl
 import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.get
 import java.io.FileInputStream
 import java.util.Properties
 
-class AndroidApplicationSignConventionPlugin: Plugin<Project> {
+class AndroidApplicationSignConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             val signingPropertiesFile = rootProject.file("signing.properties")
@@ -16,19 +14,19 @@ class AndroidApplicationSignConventionPlugin: Plugin<Project> {
 
             signingProperties.load(FileInputStream(signingPropertiesFile))
 
-            extensions.configure<ApplicationExtension> {
-                signingConfigs {
-                    create("release") {
+            extensions.configure(ApplicationExtension::class.java) {
+                with(it) {
+                    signingConfigs.create("release").apply {
                         storeFile = file(signingProperties["keystore.path"] as String)
                         storePassword = signingProperties["keystore.password"] as String
                         keyAlias = signingProperties["key.alias"] as String
                         keyPassword = signingProperties["key.password"] as String
                     }
-                }
 
-                buildTypes {
-                    release {
-                        signingConfig = signingConfigs["release"]
+                    buildTypes {
+                        release {
+                            signingConfig = signingConfigs.findByName("release")
+                        }
                     }
                 }
             }
