1e5fad3e0b365a23be5f9e05441997a7844b8508
Bob Nicholson <bobniklv@gmail.com>
2024-08-22 00:09:26 +0300

    chore: revert app sign convention plugin implementation

--- build-logic/signing/src/main/kotlin/com/yawl/AndroidApplicationSignConventionPlugin.kt
+++ build-logic/signing/src/main/kotlin/com/yawl/AndroidApplicationSignConventionPlugin.kt
@@ -10,27 +10,25 @@ class AndroidApplicationSignConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             val signingPropertiesFile = rootProject.file("signing.properties")
-            if (signingPropertiesFile.exists()) {
-                val signingProperties = Properties().apply {
-                    load(FileInputStream(signingPropertiesFile))
-                }
-                extensions.configure(ApplicationExtension::class.java) {
-                    with(it) {
-                        signingConfigs.create("release").apply {
-                            storeFile = file(signingProperties["keystore.path"] as String)
-                            storePassword = signingProperties["keystore.password"] as String
-                            keyAlias = signingProperties["key.alias"] as String
-                            keyPassword = signingProperties["key.password"] as String
-                        }
-                        buildTypes {
-                            release {
-                                signingConfig = signingConfigs.findByName("release")
-                            }
+            val signingProperties = Properties().apply {
+                load(FileInputStream(signingPropertiesFile))
+            }
+
+            extensions.configure(ApplicationExtension::class.java) {
+                with(it) {
+                    signingConfigs.create("release").apply {
+                        storeFile = file(signingProperties["keystore.path"] as String)
+                        storePassword = signingProperties["keystore.password"] as String
+                        keyAlias = signingProperties["key.alias"] as String
+                        keyPassword = signingProperties["key.password"] as String
+                    }
+
+                    buildTypes {
+                        release {
+                            signingConfig = signingConfigs.findByName("release")
                         }
                     }
                 }
-            } else {
-                println("Signing properties not found, releases will not be signed")
             }
         }
     }
