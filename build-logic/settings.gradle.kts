c06cc431585abf07d995f971850658f04970a8c6
Bob Nicholson <bobniklv@gmail.com>
2024-08-30 20:21:57 +0300

    build: add fail on project repos flag in build-logic settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -5,6 +5,7 @@ rootProject.name = "build-logic"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
