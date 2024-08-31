f413c7609cc405d64b1da49479b36e986ccdd3ad
Bob Nicholson <bobniklv@gmail.com>
2024-08-31 22:33:04 +0300

    refactor: remove testing dependencies in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,5 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.compose.material3)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-
     implementation(projects.core.designSystem)
 }
